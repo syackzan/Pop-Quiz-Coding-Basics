1ad19d6d59a41d3ee8e4e76d140275adc1c28db1
Scotty Yackzan <syackzan@gmail.com>
2021-08-15 15:32:12 -0700

    Added more functionality to Start Button - stored querySelector Variables from first page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,25 @@ console.log("Hello");
 
 var startButton = document.querySelector ("button");
 console.log(startButton);
+
+var headerOne = document.querySelector ("h1");
+console.log(headerOne);
+
+var description = document.querySelector(".description");
+console.log(description);
+
+var answerBox = document.querySelector(".answers");
+console.log(answerBox);
+
+function startQuiz (){
+    headerOne.textContent = "Question One";
+    description.style.display = "none";
+    startButton.textContent = "Answer";
+    answerBox.style.alignItems = ("flex-start");
+       
+}
+
+startButton.addEventListener ("click", function () {
+    startQuiz ();
+});
+   
