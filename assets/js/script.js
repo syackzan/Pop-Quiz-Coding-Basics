485c3060ff19d80d00bea97507cc469bf637bfd0
Scotty Yackzan <syackzan@gmail.com>
2021-08-15 16:00:45 -0700

    Created and Appended Buttons from Javascript - Added a Filler Question and Answers to test functionality - added hover functionality for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 console.log("Hello");
 
-var startButton = document.querySelector ("button");
-console.log(startButton);
+// querySelectors with Console Logs to Confirm if retreivals are workings //
+
+// Storing the Start Button into Button1 //
+var button1 = document.querySelector ("button");
+console.log(button1);
 
 var headerOne = document.querySelector ("h1");
 console.log(headerOne);
@@ -9,18 +12,36 @@ console.log(headerOne);
 var description = document.querySelector(".description");
 console.log(description);
 
-var answerBox = document.querySelector(".answers");
+var answerBox = document.querySelector(".answerBox");
 console.log(answerBox);
 
+// Creating Button Children //
+
+var button2 = document.createElement ("button");
+var button3 = document.createElement ("button");
+var button4 = document.createElement ("button");
+
+// Initial Function to open up the first question //
 function startQuiz (){
-    headerOne.textContent = "Question One";
+
+    // Changing Initial HTML Elements to Quiz Format //
+    headerOne.textContent = "Who is the greatest Coder to ever Walk the Earth?";
     description.style.display = "none";
-    startButton.textContent = "Answer";
-    answerBox.style.alignItems = ("flex-start");
-       
+    answerBox.setAttribute ("style", "align-items: flex-start"); 
+    
+    // Appending New Buttons //
+    answerBox.appendChild (button2);
+    answerBox.appendChild (button3);
+    answerBox.appendChild (button4);
+
+    // Adding Text to New Appended Buttons //
+    button1.textContent = "Steve Jobs";
+    button2.textContent = "Mark Zuckerberg";
+    button3.textContent = "Elon Musk";
+    button4.textContent = "Jeff Bezos"
 }
 
-startButton.addEventListener ("click", function () {
+button1.addEventListener ("click", function () {
     startQuiz ();
 });
    
