db466390cdbe10b2906e8136c58d60606e8e833a
Scotty Yackzan <syackzan@gmail.com>
2021-08-17 17:15:30 -0700

    Created Individual Elements for every Question's Set of Boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,12 @@ var count = document.querySelector(".count");
 var timerHTML = document.querySelector(".timer");
 
 // Creating Button Children //
+var buttonBox1 = document.createElement ("section");
+var buttonBox2 = document.createElement ("section");
+var buttonBox3 = document.createElement ("section");
+var buttonBox4 = document.createElement ("section");
+var buttonBox5 = document.createElement ("section");
+var enterInitialsBox = document.createElement ("section")
 var button1 = document.createElement ("button");
 var button2 = document.createElement ("button");
 var button3 = document.createElement ("button");
@@ -67,7 +73,7 @@ function renderHighS () {
         var oneScore = allScores[i];
     
         var li = document.createElement("li");
-        li.textContent = "Initials: " + oneScore.initials + " Time: " + oneScore.timer;
+        li.textContent = "Initials: " + oneScore.initials + " Time: " + oneScore.score;
 
         ol.appendChild(li);
       }
@@ -83,7 +89,7 @@ function renderHighS () {
 
 // Updating Score //
 function updatingScore (input){
-
+    console.log(timer);
     if (input != null){
         var userInfo = {
             initials: input.value,
@@ -118,34 +124,32 @@ function highScoreForm (){
 
 // Changing to Save Score Form //
 function changeForm (){
-    answerBox.setAttribute ("style", "flex-direction: row; justify-content: flex-start; border-bottom: solid 1px gray");
-    answerBox.style.justifyContent = "flex-start";
-    answerBox.style.flexDirection = "row";
+    userBox.appendChild(enterInitialsBox);
+    enterInitialsBox.setAttribute ("style", "display: flex; flex-direction: row; justify-content: flex-start; padding: 10px; border-bottom: solid 1px gray");
+    enterInitialsBox.style.justifyContent = "flex-start";
+    enterInitialsBox.style.flexDirection = "row";
     description.style.display = "visible";
     description.textContent = "Your Final score is " + timer;
 
-    answerBox.appendChild (enterIn);
+    enterInitialsBox.appendChild (enterIn);
     enterIn.textContent = "Enter Your Initials:";
 
-    answerBox.appendChild (input);
+    enterInitialsBox.appendChild (input);
     input.setAttribute("type", "text");
     input.setAttribute ("name", "FullName");
     input.setAttribute ("placeholder", "Initials");
     input.setAttribute ("style", "margin: 0px 10px");
 
-    answerBox.appendChild (submitB);
+    enterInitialsBox.appendChild (submitB);
     submitB.setAttribute ("type", "submit");
     submitB.setAttribute ("value", "Submit");
     submitB.setAttribute ("style", "margin: 0px 10px; font-size: 24px; background-color: purple; color: white");
 
-    console.log (input.value); 
+    return input;
 }
 
 // View High Scores Menu //
 function viewHighScores (input){
-    
-    //Load Stored High Scores.
-    //renderHighS ();
     
     // Removing Buttons From HTML //
     highScoreForm ();
@@ -158,15 +162,14 @@ function viewHighScores (input){
 
     clearScoreB.addEventListener("click", function (){
         localStorage.clear();
-        event.stopPropagation();
+        description.textContent = "";
         return;
     })
 }
 
 // End saveScore Menu //
-function storeScore (){
+function storeScore (event){
     var element = event.target;
-    console.log(element);
 
     if (element.matches("button")){
 
@@ -178,148 +181,155 @@ function storeScore (){
         button3.style.display = "none";
         button4.style.display = "none";
         
-        changeForm ();
-    }
+        var returnElementInput = changeForm();
+        console.log(input);
+        var userNameInput = returnElementInput.value;
+        console.log(userNameInput);
 
-    event.stopPropagation()
-    submitB.addEventListener("click", function (){
-    viewHighScores (input);
-    })
-    
+        submitB.addEventListener("click", function (){
+            viewHighScores ();
+            })
+    }
 }
 
 // Question 5 Function //
-function questionFive (){
+function questionFive (event){
     var element = event.target;
     console.log(element);
 
     if (element.matches("button")){
-        
-    // Adding New Question & Answer Text //
-    headerOne.textContent = "Which country is home to Samurai's?";
-    button1.textContent = "China";
-    button2.textContent = "Japan";
-    button3.textContent = "Canada";
-    button4.textContent = "Korea";
 
-    // True False Answers //
-    button1.value = "false";
-    button2.value = "true";
-    button3.value = "false";
-    button4.value = "false";
-    }
+        //Erasing Buttons & Appending New Set
+        buttonBox4.innerHTML = "";
+        userBox.appendChild(buttonBox5);
+        buttonBox5.setAttribute ("style", "display: flex; flex-direction: column; justify-content: flex-start; align-items: flex-start; border-bottom: solid 1px gray;");
+        appButtons5();
+            
+        timerCheck(element);  
 
-        event.preventDefault();
-    event.stopPropagation()
-    answerBox.addEventListener("click", function (){
-        storeScore ();
-    })
-    
+        // Adding New Question & Answer Text //
+        headerOne.textContent = "Which country is home to Samurai's?";
+        button1.textContent = "China";
+        button2.textContent = "Japan";
+        button3.textContent = "Canada";
+        button4.textContent = "Korea";
+
+        // True False Answers //
+        button1.value = "false";
+        button2.value = "true";
+        button3.value = "false";
+        button4.value = "false";
+
+        buttonBox5.addEventListener("click", function (event){
+            storeScore (event);
+        })
+    } 
 }
 
 
 // Question 4 Function //
-function questionFour (){
+function questionFour (event){
     var element = event.target;
+    console.log(element);
 
     if (element.matches("button")){
-        
-    // Adding New Question & Answer Text //
-    headerOne.textContent = "How many Fingers do we have?";
-    button1.textContent = "Five";
-    button2.textContent = "Twenty";
-    button3.textContent = "Ten";
-    button4.textContent = "One";
 
-    // True False Answers //
-    button1.value = "false";
-    button2.value = "false";
-    button3.value = "true";
-    button4.value = "false";
-    }
+        //Erasing Buttons & Appending New Set
+        buttonBox3.innerHTML = "";
+        userBox.appendChild(buttonBox4);
+        buttonBox4.setAttribute ("style", "display: flex; flex-direction: column; justify-content: flex-start; align-items: flex-start; border-bottom: solid 1px gray;");
+        appButtons4();
+            
+        timerCheck(element);    
 
-    event.preventDefault();
-    event.stopPropagation()
-    answerBox.addEventListener("click", function (){
-        questionFive ();
-    })
-    
+        // Adding New Question & Answer Text //
+        headerOne.textContent = "How many Fingers do we have?";
+        button1.textContent = "Five";
+        button2.textContent = "Twenty";
+        button3.textContent = "Ten";
+        button4.textContent = "One";
+
+        // True False Answers //
+        button1.value = "false";
+        button2.value = "false";
+        button3.value = "true";
+        button4.value = "false";
+
+        buttonBox4.addEventListener("click", function (event){
+            questionFive (event);
+        });
+    } 
 }
 
 // Question 3 Function //
-function questionThree (){
+function questionThree (event){
     var element = event.target;
+    console.log(element);
 
     if (element.matches("button")){
-        
-    // Adding New Question & Answer Text //
-    headerOne.textContent = "What state is Mount Rushmore in?";
-    button1.textContent = "California";
-    button2.textContent = "Minnesota";
-    button3.textContent = "South Dakota";
-    button4.textContent = "North Dakota";
 
-    // True False Answers //
-    button1.value = "false";
-    button2.value = "false";
-    button3.value = "false";
-    button4.value = "true";
-    }
+        //Erasing Buttons & Appending New Set
+        buttonBox2.innerHTML = "";
+        userBox.appendChild(buttonBox3);
+        buttonBox3.setAttribute ("style", "display: flex; flex-direction: column; justify-content: flex-start; align-items: flex-start; border-bottom: solid 1px gray;");
+        appButtons3();
+            
+        timerCheck(element);
 
-    event.preventDefault();
-    event.stopPropagation()
-    answerBox.addEventListener("click", function (){
-        questionFour ();
-    })
-    
+        // Adding New Question & Answer Text //
+        headerOne.textContent = "What state is Mount Rushmore in?";
+        button1.textContent = "California";
+        button2.textContent = "Minnesota";
+        button3.textContent = "South Dakota";
+        button4.textContent = "North Dakota";
+
+        // True False Answers //
+        button1.value = "false";
+        button2.value = "false";
+        button3.value = "false";
+        button4.value = "true";
+
+        buttonBox3.addEventListener("click", function (event){
+            questionFour (event);
+        }); 
+    } 
 }
 
 // Question 2 & Set Up for Allow Box Selection inside Div Area//
 function questionTwo (event){
     var element = event.target;
+    console.log(element);
 
     if (element.matches("button")){
 
+        buttonBox1.innerHTML = "";
+        userBox.appendChild(buttonBox2);
+        buttonBox2.setAttribute ("style", "display: flex; flex-direction: column; justify-content: flex-start; align-items: flex-start; border-bottom: solid 1px gray;");
+        appButtons2(); 
+
+        timerCheck(element);
+
         userBox.appendChild (reveal);
         reveal.setAttribute ("style", "font-size: 16px; color: gray; font-style: italic;")
-       
-        if (element.value == "true"){
-            scoreCount++;
-            count.textContent = "Answered Correct: " + scoreCount;
-            reveal.textContent = "Correct!";
-        } else {
-            
-            if (timer >= 10){
-                timer = timer - 10;
-                timerHTML.textContent = "Timer: " + timer;
-                reveal.textContent = "Wrong!";
-            } else {
-                timer = 0;
-                timerHTML.textContent = "Timer: " + timer;
 
-            }
-            
-        } 
+        // Adding New Question & Answer Text //
+        headerOne.textContent = "Are Tomato's Fruits or Vegetables?";
+        button1.textContent = "Fruits";
+        button2.textContent = "Vegetables";
+        button3.textContent = "Shut Up";
+        button4.textContent = "All of the Above";
+
+        // True False Answers //
+        button1.value = "true";
+        button2.value = "false";
+        button3.value = "false";
+        button4.value = "false";
+
         
-    // Adding New Question & Answer Text //
-    headerOne.textContent = "Are Tomato's Fruits or Vegetables?";
-    button1.textContent = "Fruits";
-    button2.textContent = "Vegetables";
-    button3.textContent = "Shut Up";
-    button4.textContent = "All of the Above";
-
-    // True False Answers //
-    button1.value = "true";
-    button2.value = "false";
-    button3.value = "false";
-    button4.value = "false";
-
-    
-    event.stopPropagation()
-    answerBox.addEventListener("click", function (){
-        questionThree ();
-    })
-    }  
+        buttonBox2.addEventListener("click", function (event){
+        questionThree (event);
+    });
+    }      
 }
 
 // Initial Function to open up the first question //
@@ -327,38 +337,33 @@ function startQuiz (){
     
     
     // Changing Initial HTML Elements to Quiz Format //
-    headerOne.textContent = "Who is the greatest Coder to ever Walk the Earth?";
+    headerOne.textContent = "Who is the greatest business man to ever Walk the Earth?";
     description.textContent = " ";
     description.setAttribute ("style", "margin: 0px; border: none");
     startButton.style.display = "none";
     count.textContent = "Answered Correct: " + scoreCount;
     timerHTML.textContent = "Timer: " + timer;
-    answerBox.setAttribute ("style", "align-items: flex-start; border-bottom: solid 1px gray;"); 
+    answerBox.style.display = "none";
+    userBox.appendChild(buttonBox1);
+    buttonBox1.setAttribute ("style", "display: flex; flex-direction: column; justify-content: flex-start; align-items: flex-start; border-bottom: solid 1px gray;"); 
     
     
     // Appending New Buttons //
-    answerBox.appendChild (button1);
-    answerBox.appendChild (button2);
-    answerBox.appendChild (button3);
-    answerBox.appendChild (button4);
+    appButtons1();
 
     // Adding Text to New Appended Buttons //
     button1.textContent = "Steve Jobs";
     button2.textContent = "Mark Zuckerberg";
     button3.textContent = "Elon Musk";
     button4.textContent = "Jeff Bezos";
-    console.log(button1);
-    console.log(button2);
 
     // True False Answers //
     button1.value = "false";
-    button2.value = "true";
+    button2.value = "false";
     button3.value = "false";
-    button4.value = "false";
-
+    button4.value = "true";
     
-    //event.stopPropagation();
-    answerBox.addEventListener("click", function (event){
+    buttonBox1.addEventListener("click", function (event){
         questionTwo (event);
     })
     
@@ -370,3 +375,63 @@ startButton.addEventListener ("click", function(){
     timerCountdown();
   });
    
+
+// Function to Add to Count or Takeaway from Time if answer is right or wrong //
+function timerCheck(element){
+    if (element.value == "true"){
+        scoreCount++;
+        count.textContent = "Answered Correct: " + scoreCount;
+        reveal.textContent = "Correct!";
+    } else {
+        
+        if (timer >= 10){
+            timer = timer - 10;
+            timerHTML.textContent = "Timer: " + timer;
+            reveal.textContent = "Wrong!";
+        } else {
+            timer = 0;
+            timerHTML.textContent = "Timer: " + timer;
+
+        }
+    }
+}
+
+// All Append Buttons to new Button Boxes after each subsequent Question //
+function appButtons1(){
+buttonBox1.appendChild (button1);
+buttonBox1.appendChild (button2);
+buttonBox1.appendChild (button3);
+buttonBox1.appendChild (button4);
+}
+
+function appButtons2(){
+    buttonBox2.innerHTML = "";
+    buttonBox2.appendChild (button1);
+    buttonBox2.appendChild (button2);
+    buttonBox2.appendChild (button3);
+    buttonBox2.appendChild (button4);
+}
+
+function appButtons3(){
+    buttonBox3.innerHTML = "";
+    buttonBox3.appendChild (button1);
+    buttonBox3.appendChild (button2);
+    buttonBox3.appendChild (button3);
+    buttonBox3.appendChild (button4);
+}
+
+function appButtons4(){
+    buttonBox4.innerHTML = "";
+    buttonBox4.appendChild (button1);
+    buttonBox4.appendChild (button2);
+    buttonBox4.appendChild (button3);
+    buttonBox4.appendChild (button4);
+}
+
+function appButtons5(){
+    buttonBox5.innerHTML = "";
+    buttonBox5.appendChild (button1);
+    buttonBox5.appendChild (button2);
+    buttonBox5.appendChild (button3);
+    buttonBox5.appendChild (button4);
+}
